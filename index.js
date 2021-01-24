c3ea539974e8a8f127c1d863813341ce06cbb63f
bhavya-verma-knoldus <bhavya.verma@knoldus.com>
2021-01-24 17:58:15 +0530

    Checking 18th time

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello from knoldus\n'
+	const msg2 = 'Assignment completed\n'
 	res.end(msg2);
 });
 
